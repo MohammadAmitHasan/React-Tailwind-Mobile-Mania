d071a31a878c0a91ffc75add178d740ed30b802d
Muhammad Amit Hasan <bhamithasan@gmail.com>
2022-04-03 15:50:38 +0600

    Route seted up

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -5,11 +5,6 @@ import Mobiles from '../Mobiles/Mobiles';
 const Home = () => {
     const navigate = useNavigate();
 
-    const allButtonHandle = () => {
-        const path = '/home/All';
-        navigate(path);
-    }
-
     const appleButtonHandle = () => {
         const path = `/home/Apple`;
         navigate(path);
@@ -24,10 +19,11 @@ const Home = () => {
         <div className='grid grid-cols-4 container mx-auto'>
             <div className='col-span-3 mr-5'>
                 <div className='text-center mt-6'>
-                    <button onClick={allButtonHandle} className='bg-blue-300 py-1 px-3 m-2 rounded-lg'>All</button>
+                    <Link to={'/home/All'}>
+                        <button className='bg-blue-300 py-1 px-3 m-2 rounded-lg'>All</button>
+                    </Link>
                     <button onClick={samsungButtonHandle} className='bg-blue-300 py-1 px-3 m-2 rounded-lg'>Samsung</button>
                     <button onClick={appleButtonHandle} className='bg-blue-300 py-1 px-3 m-2 rounded-lg'>Apple</button>
-                    <Link to={'/home/All'}>All</Link>
                 </div>
                 <Outlet></Outlet>
                 <Mobiles></Mobiles>
